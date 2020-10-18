1da3288e5e99ecaab9904047a5a216a62bae4596
kimkim0814 <tnryj505@gmail.com>
2020-10-18 19:38:16 +0900

    add helper UIKIT base_url

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -1,5 +1,11 @@
-<?php 
+<?php
+
 class Post extends CI_Controller {
+
+  public function __construct(){
+    $this->load->helper("url_helper");
+
+  }
   public function index() {
     $this->load->view('template/header');
   }
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,4 +1,19 @@
-<?php 
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="<?= base_url() ?>css/uikit.min.css" type= "text/css">
+</head>
+<body>
 
-?>
-header
+<div class="uk-grid">
+    <div class="uk-width-1-3">.uk-width-1-3</div>
+    <div class="uk-width-1-3">.uk-width-1-3</div>
+    <div class="uk-width-1-3">.uk-width-1-3</div>
+</div>
+
+
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/uikit.min.js"></script>
+</body>
+</html>
