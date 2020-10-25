1bd3fb40ee45b2ff3a598fee81415d2706abe105
kimkim0814 <tnryj505@gmail.com>
2020-10-25 23:57:06 +0900

    add data

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -1,18 +1,24 @@
 <?php
 
-class Post extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        //ヘルパー
-        $this->load->helper('url');
-        $this->load->helper('form');
-        //ライブラリー
-        $this->load->library('form_validation');
-    }
-    public function index()
-    {
-        $this->load->view('template/header');
-    }
+class Post extends CI_Controller {
+
+  public function __construct(){
+    parent::__construct();
+    //ヘルパー
+    $this->load->helper('url');
+    $this->load->helper('form');
+    //ライブラリー
+    $this->load->library('form_validation');
+    //モデル
+    $this->load->model('post_model');
+
+  }
+  public function index() {
+    $this->load->view('template/header');
+    $data['post'] = $this->post_model->get_news();
+  }
+  public function view(){
+    $data['post_item'] = $this->post_model->get_news($slug);
 }
+}
+?>
