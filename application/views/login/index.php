0781539f279ad2a8a784a218fa61a56d341fd1f6
kimkim0814 <tnryj505@gmail.com>
2020-11-09 00:30:37 +0900

    セッションがない場合redirect

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,6 +1,6 @@
-<form action = "<?= base_url(); ?>login/login_validation">
+<?php echo form_open('login/login_validation'); ?>
 
-    <div class="uk-margin">
+   <div class="uk-margin">
         <div class="uk-inline">
             <span class="uk-form-icon" uk-icon="icon: user"></span>
             <input class="uk-input" type="text" name="email"  value="<?= $this->input->post("email")?>"> 
@@ -13,6 +13,6 @@
             <input class="uk-input" type="password" name="password">
         </div>
     </div>
-  <button type = "submit" class="uk-button uk-button-primary">Login</button>
+  <button type = "submit" class="uk-button uk-button-primary">Login</button>  
 
 </form>
