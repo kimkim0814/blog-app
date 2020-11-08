9ccc17a21b662cc2e89359c3f24f57f6e94fb80a
kimkim0814 <tnryj505@gmail.com>
2020-11-09 00:31:08 +0900

    インデント　セッション

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,48 +1,60 @@
 <?php
 
-class Blog extends CI_Controller {
-
-  public function __construct(){
-    parent::__construct();
-    //ヘルパー
-    $this->load->helper('url');
-    $this->load->helper('form');
-    //ライブラリー
-    $this->load->library('form_validation');
-    //モデル
-    $this->load->model('blog_model');
-
-  }
-  public function index() {
-   
-    $data['blog'] = $this->blog_model->get_blog();
-    $this->load->view('template/header');
-    $this->load->view('blog/index',$data);
-  }
-
-  public function view($id = NULL){
-    $data['blog_item'] = $this->blog_model->get_blog($id);
-    $this->load->view('template/header');
-    $this->load->view('blog/view',$data);
-}
-
-
-  public function create(){
-    $this->load->helper('form');
-    $this->load->library('form_validation');
-
-    $this->form_validation->set_rules('title', 'Title', 'required');
-    $this->form_validation->set_rules('description', 'description', 'required');
-
-    if($this->form_validation->run() === FALSE ){
-      $this->load->view('template/header');
-      $this->load->view('blog/create');
-    }else{
-      $this->blog_model->set_blog();
-      echo '<script type="text/javascript">alert("投稿が完了しました");</script>';
-      echo '<script>location.href= "/blog-app"; </script>';
-
+class Blog extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //ヘルパー
+        $this->load->helper('url');
+        $this->load->helper('form');
+        //ライブラリー
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        //モデル
+        $this->load->model('blog_model');
     }
-  }
+    public function index()
+    {
+        $data['hoge'] = $this->session->userdata('hoge');
+        $data['blog'] = $this->blog_model->get_blog();
+
+        if ($this->session->userdata("is_logged_in")) {	//ログインしている場合の処理
+            $this->load->view('template/header');
+            $this->load->view('blog/index', $data);
+
+
+        } else {									//ログインしていない場合の処理
+            redirect("login/index");
+        }
+
+        
+        
+    }
+
+    public function view($id = null)
+    {
+        $data['blog_item'] = $this->blog_model->get_blog($id);
+        $this->load->view('template/header');
+        $this->load->view('blog/view', $data);
+    }
+
+
+    public function create()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('description', 'description', 'required');
+
+        if ($this->form_validation->run() === false) {
+            $this->load->view('template/header');
+            $this->load->view('blog/create');
+        } else {
+            $this->blog_model->set_blog();
+            echo '<script type="text/javascript">alert("投稿が完了しました");</script>';
+            echo '<script>location.href= "/blog-app"; </script>';
+        }
+    }
 }
-?>
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,35 +1,63 @@
 <?php
 
-class Login extends CI_Controller {
-  public function __construct(){
-    parent::__construct();
-    //ヘルパー
-    $this->load->helper('url');
-    $this->load->helper('form');
-    //ライブラリー
-    $this->load->library('form_validation');
-    //モデル
-    $this->load->model('user_model');
+class Login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //ヘルパー
+        $this->load->helper('url');
+        $this->load->helper('form');
+        //ライブラリー
+        $this->load->library('form_validation');
+        $this->load->library('session');
 
-  }
-  public function index(){
-    $this->load->view('template/header');
-    $this->load->view('login/index');
-  }
-  public function login_validation(){
-
-    $this->form_validation->set_rules("email","メール","required|trim");
-    $this->form_validation->set_rules("password", "パスワード", "required|md5|trim");
-
-    if($this->form_validation->run()){
-      redirect("blog/index");
-    }else{
-      echo "<script>alert('正しいメールアドレスとパスワードを入力してください')</script>";
-      $this->load->view('template/header');
-      $this->load->view("login/index");
+        //モデル
+        $this->load->model('user_model');
     }
-  }
+    public function index()
+	{
+		$this->login();
+	}
 
+	public function login()
+	{
+
+		$this->load->view('template/header' );
+		$this->load->view('login/index');
+
+	}
+
+    public function login_validation()
+    {
+        $this->form_validation->set_rules("email", "メール", "required|trim|callback_validate_credentials");
+        $this->form_validation->set_rules("password", "パスワード", "required|md5|trim");
+
+        if ($this->form_validation->run()) {
+            $data = array(
+            "email" => $this->input->post("email"),
+            "is_logged_in" => 1
+          );
+            $this->session->set_userdata($data);
+
+            redirect("blog/index");
+        } else {
+            echo "<script>alert('正しいメールアドレスとパスワードを入力してください')</script>";
+            $this->load->view('template/header');
+            $this->load->view("login/index");
+        }
+    }
+
+    public function validate_credentials()
+    {		//Email情報がPOSTされたときに呼び出されるコールバック機能
+        $this->load->model("user_model");
+
+        if ($this->user_model->can_log_in()) {	//ユーザーがログインできたあとに実行する処理
+            return true;
+        } else {					//ユーザーがログインできなかったときに実行する処理
+            $this->form_validation->set_message("validate_credentials", "ユーザー名かパスワードが異なります。");
+            return false;
+        }
+    }
 
 }
-?>
