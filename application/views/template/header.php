ef4ebc7fae5f1b048fcfbc4b0f8fc187a4286d71
kimkim0814 <tnryj505@gmail.com>
2020-11-09 00:49:28 +0900

    change header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,9 +5,36 @@
     <link rel="stylesheet" href="<?= base_url() ?>css/blog.css" type= "text/css">
 </head>
 <body>
-        <div>
-        <h1 class="uk-heading-divider" ><a href= "<?=base_url();?>" style = "text-decoration: none; color:black;">初めてのブログサイト</a></h1>
-</div>
+
+<nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" href="<?= base_url() ?>">BLOG-APP</a>
+
+        <ul class="uk-navbar-nav">
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Features
+                </a>
+            </li>
+        </ul>
+
+        <div class="uk-navbar-item">
+            <div>Some <a href="#">Link</a></div>
+        </div>
+
+        <div class="uk-navbar-item">
+            <form action="javascript:void(0)">
+                <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
+                <button class="uk-button uk-button-default">Button</button>
+            </form>
+        </div>
+
+    </div>
+</nav>
+        
+
    
 
     <script src="<?= base_url() ?>js/uikit.min.js"></script>
