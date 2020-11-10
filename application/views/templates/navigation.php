fe8f196c185372bb13fe0aa0f2103fbe2cfe749b
prismistim <prismistim@hotmail.com>
2020-11-10 22:48:45 +0900

    change: ヘッダー・フッダーなどの分離と記述の整形

---
+++ application/views/templates/navigation.php
@@ -0,0 +1,11 @@
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+        <a href="" class="uk-navbar-item uk-logo">blog-app</a>
+    </div>
+
+    <div class="uk-navbar-right">
+        <div class="uk-navbar-item">
+            <a href="blog/create" uk-icon="icon: file-edit"></a>
+        </div>
+    </div>
+</nav>
