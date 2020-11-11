8e2c89133b7118a3826d943ee860debde543ad4b
kimkim0814 <tnryj505@gmail.com>
2020-11-11 21:26:23 +0900

    投稿ページにbulma導入

--- application/views/blog/create.php
+++ application/views/blog/create.php
@@ -1,21 +1,19 @@
-
-
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('blog/create'); ?>
-<fieldset class="uk-fieldset" style="width:400px;">
-    <label for="title">Title</label>
-    <div class="uk-margin">
-            <input class="uk-input" type="text" name="title" >
-    </div>
-   
-    <label for="text">詳細</label>
-    <div class="uk-margin">
-            <textarea class="uk-textarea" name="description" rows="5" placeholder="Textarea"></textarea>
+<div style="width:400px;">
+        <div class="field">
+                <label class="label">タイトル</label>
+                <div class="control">
+                        <input class="input" type="text" name="title" placeholder="input title">
+                </div>
         </div>
-
-     <button class="uk-button uk-button-primary" type = "submit" onclick="location.href='blog/create'">投稿画面へ</button>
-
-
-</fileset>
-</form>
+        <div class="field">
+                <label class="label">詳細</label>
+                <div class="control">
+                        <textarea class="textarea"  name = "description" placeholder="description"></textarea>
+                </div>
+        </div>
+        <button class="button is-link" type="submit" onclick="location.href='blog/create'">投稿画面へ</button>
+        </fileset>
+        </form>
